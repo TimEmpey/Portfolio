50defed06814011a4efa64dd922a736b5e9c77d9
Tim Empey <timothyempey@gmail.com>
2022-11-30 11:32:35 -0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/images/'
+              outputPath: '/images/'
             }
           }
         ]
